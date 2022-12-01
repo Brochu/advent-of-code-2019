4215833bbc467ce363487449d77c9e89353056bb
Alex Brochu <alexbrochu1@gmail.com>
2022-11-30 22:15:30 -0500

    [WORK] Done with Day 6, part 1 and 2

--- src/day6.rs
+++ src/day6.rs
@@ -6,8 +6,7 @@ fn main() {
 }
 
 fn build_tree(data: &str) -> HashMap<String, Vec<String>> {
-    let mut tree = HashMap::new();
-    tree.insert(String::from(""), vec![]);
+    let mut tree = HashMap::<String, Vec<String>>::new();
 
     data
         .lines()
@@ -24,38 +23,73 @@ fn build_tree(data: &str) -> HashMap<String, Vec<String>> {
             };
         });
 
-    tree.remove_entry(&String::from(""));
     return tree;
 }
 
-fn calc_orbit(tree: &HashMap<String, Vec<String>>) -> usize {
-    let mut depth: usize = 0;
-    return calc_orbit_internal(tree, String::from("COM"), &mut depth);
+fn build_parents(tree: &HashMap<String, Vec<String>>) -> HashMap<String, String> {
+    let mut parents = HashMap::<String, String>::new();
+
+    tree.iter()
+        .for_each(|e| {
+            e.1.iter().for_each(|c| {
+                parents.insert(c.to_string(), e.0.to_string());
+            });
+        });
+
+    parents.insert(String::from("COM"), String::from(""));
+    return parents;
 }
 
-fn calc_orbit_internal(tree: &HashMap<String, Vec<String>>, current: String, depth: &mut usize) -> usize {
-    //TODO: Need to debug this
-    let count = match tree.get(&current) {
-        Some(children) => {
-            *depth += 1;
-            for c in children {
-                calc_orbit_internal(tree, c.to_string(), depth);
-            }
-            *depth
-        },
-        None => *depth,
-    };
+fn calc_orbit(tree: &HashMap<String, Vec<String>>) -> usize {
+    return calc_orbit_internal(tree, String::from("COM"), 0);
+}
 
-    println!("[ORBIT_CALC] [{:?}]={:?}", current, count);
-    return count;
+fn calc_orbit_internal(tree: &HashMap<String, Vec<String>>, current: String, depth: usize) -> usize {
+    return match tree.get(&current) {
+        Some(children) => {
+            let subtotal: usize = children.iter()
+                .map(|c| calc_orbit_internal(tree, c.to_string(), depth+1))
+                .sum();
+
+            subtotal + depth
+        },
+        None => depth,
+    };
 }
 
 fn run_part1() -> usize {
-    let tree = build_tree(include_str!("../data/day6.example"));
-
+    let tree = build_tree(include_str!("../data/day6.input"));
     return calc_orbit(&tree);
 }
 
 fn run_part2() -> usize {
+    let tree = build_tree(include_str!("../data/day6.input"));
+    let parents = build_parents(&tree);
+
+    //println!("{:?}", tree);
+    //println!("{:?}", parents);
+
+    let mut mybacktrack: Vec<String> = Vec::new();
+
+    let mut current = String::from("YOU");
+    while current.len() > 0 {
+        current = parents.get(&current).unwrap().to_string();
+        mybacktrack.push(current.to_string());
+    }
+
+    current = parents.get(&String::from("SAN")).unwrap().to_string();
+    let mut count = 0;
+
+    while current.len() > 0 {
+        let pos = mybacktrack.iter().position(|n| *n == current);
+        match pos {
+            Some(i) => return i + count,
+            None => {
+                count += 1;
+                current = parents.get(&current).unwrap().to_string();
+            }
+        }
+    }
+
     return 0;
 }
