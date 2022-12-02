79d2ede57a67f2884b11be655a9f3e1e18ee784d
Alex Brochu <alexbrochu1@gmail.com>
2022-12-01 23:52:27 -0500

    [W] Skipping day 7, don't know how to write to stdin

--- src/day7.rs
+++ src/day7.rs
@@ -4,11 +4,13 @@ fn main() {
 }
 
 fn run_part1() -> i64 {
-    let mem: Vec<i64> = include_str!("../data/day7.input")
+    let mem: Vec<i64> = include_str!("../data/day7.example")
         .split(",")
         .map( |val| val.trim().parse::<i64>().unwrap() )
         .collect();
 
-    println!("{:?}", mem);
+    //TODO: I DO NOT KNOW HOW TO QUEUE SOME DATA IN STDIN
+    // HOW DO I PROGRAMMATICALLY SEND DATA TO STDIN
+
     return 0;
 }
