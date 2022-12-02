08827295265a20047414154d0892b9e32699cfc1
Alex Brochu <alexbrochu1@gmail.com>
2022-12-02 17:34:50 -0500

    [S] Prepare for day 9

---
+++ src/day9.rs
@@ -0,0 +1,15 @@
+fn main() {
+    //let ex = include_str!("../data/day9.example");
+    //let input = include_str!("../data/day9.input");
+
+    println!("[Day9] part 1 = {}", run_part1());
+    println!("[Day9] part 2 = {}", run_part2());
+}
+
+fn run_part1() -> u64 {
+    return 0;
+}
+
+fn run_part2() -> u64 {
+    return 0;
+}
