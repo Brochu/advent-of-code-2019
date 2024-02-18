3f33bcfbba486697664e7a0610ab635cbdfff005
Brochu <alexbrochu1@gmail.com>
2024-02-18 17:40:17 -0500

    [DEV] WIP: Restarted day10

--- src/day10.rs
+++ src/day10.rs
@@ -1,64 +1,43 @@
-#[derive(Debug)]
-enum State {
+use std::fmt::Display;
+
+enum Node {
     Empty,
     Asteroid,
 }
-
-fn display_state(state: &State) -> char {
-    match state {
-        State::Empty => '.',
-        State::Asteroid => '#',
+impl Display for Node {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let char = match self {
+            Node::Empty => ".",
+            Node::Asteroid => "#",
+        };
+        write!(f, "{}", char)
     }
 }
 
-#[derive(Debug)]
 struct Map {
     n: usize,
-    states: Vec<State>,
+    nodes: Vec<Node>,
 }
-
-fn print_map(map: &Map) {
-    map.states[..]
-        .chunks(map.n)
-        .for_each(|st| {
-            st.iter().for_each(|s| print!("{:?}", display_state(s)));
-            println!();
-        });
-
-    println!();
-}
-
-fn idx_to_coords(map: &Map, idx: usize) -> (i64, i64) {
-    return ((idx / map.n) as i64, (idx % map.n) as i64);
+impl Display for Map {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let output = self.nodes[..].chunks(self.n)
+            .fold(String::new(), |acc, c| {
+                let line = c.iter().fold(String::new(), |l, n| {
+                    let char = match n {
+                        Node::Empty => ".",
+                        Node::Asteroid => "#",
+                    };
+                    format!("{}{}", l, char)
+                });
+                format!("{}\n{}", acc, line)
+            });
+        writeln!(f, "{}", output)
+    }
 }
 
 fn parse_input() -> Map {
-    let mut size = 0;
-    let map_states = include_str!("../data/day10.example")
-        .lines()
-        .flat_map(|l| {
-            size = l.len(); // Store the length of one line, to switch between idx and coords later
-            l.chars()
-        })
-        .map(|c| {
-            match c {
-                '.' => State::Empty,
-                '#' => State::Asteroid,
-                _ => panic!("[Day10] Could not parse map state"),
-            }
-        })
-        .collect::<Vec<State>>();
-
-    return Map { n: size, states: map_states };
-}
-
-fn find_gcd(mut a: i64, mut b: i64) -> i64 {
-    while b != 0 {
-        let t = b;
-        b = a % b;
-        a = t;
-    }
-    return a;
+    let input = include_str!("../data/day10.example");
+    return Map { n: 0, nodes: vec![] };
 }
 
 fn main() {
@@ -68,38 +47,8 @@ fn main() {
     //println!("[Day10] part 2 = {}", run_part2(&map));
 }
 
-            //match st {
-            //    State::Empty => None,
-            //    State::Asteroid => Some(st),
-            //}
 fn run_part1(map: &Map) -> u64 {
-    print_map(&map);
-
-    let asteroids = map.states.iter()
-        .enumerate()
-        .filter_map(|(idx, st)| {
-            match st {
-                State::Empty => None,
-                State::Asteroid => Some((idx, idx_to_coords(&map, idx))),
-            }
-        })
-        .collect::<Vec<(usize, (i64, i64))>>();
-
-    let checks = asteroids.iter()
-        .flat_map(|(i0, a0)| {
-            asteroids.iter().filter_map(move |(i1, a1)| {
-                if i0 != i1 {
-                    Some((a0, a1))
-                }
-                else {
-                    None
-                }
-            })
-        });
-
-    checks.for_each(|(a0, a1)| {
-        println!("{:?} vs. {:?}", a0, a1);
-    });
+    println!("{}", map);
     return 0;
 }
 
