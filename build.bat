a3f58a9d3e1640dfef6fd521c36a88060757c1f9
Brochu <alexbrochu1@gmail.com>
2025-04-26 00:44:23 -0400

    [SETUP] It's ok but I need to rework cond. compilation

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-rustc --cfg day%1 -o AoC2019.exe src/main.rs
+@echo off
+rustc --cfg day%1 --cfg %2 -o AoC2019.exe src/main.rs
--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,11 @@
 pub fn solve() {
-    println!("SOLVING FOR DAY 1");
+    #[cfg(EXAMPLE)] let input = include_str!("../data/day1.example");
+    #[cfg(FULL)] let input = include_str!("../data/day1.input");
+
+    let p1: i32 = input
+        .lines()
+        .map(|s| s.parse::<i32>().unwrap() )
+        .map(|i| (i/3) - 2 )
+        .sum();
+    println!("[D1] Part 1 -> {}", p1);
 }
